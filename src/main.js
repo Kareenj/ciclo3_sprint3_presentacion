9e21a18ef6c3df9ffa8bc67dd562917db3f77137
Santiago <santiagojaramillop@outlook.com>
2020-12-13 11:53:39 -0500

    Instalacion router.js y Bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,12 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import vueRouter from 'vue-router'
+import router from './router'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(vueRouter)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
